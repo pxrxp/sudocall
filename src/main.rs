83e95ff10034adf7c1caf10e92e027f5b7ee9e9d
pxrxp <187556078+pxrxp@users.noreply.github.com>
2025-11-19 23:04:57 +0545

    Add simple plot functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod terminal;
 
-fn main() {
-    let (rows, cols) = terminal::get_size().expect("Terminal isn't supported.");
-    println!("{}, {}", rows + 1, cols + 1);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    terminal::hide_cursor();
+    ctrlc::set_handler(move || {
+        terminal::show_cursor();
+        std::process::exit(0);
+    })?;
+
+    loop {
+        terminal::clear();
+        terminal::plot(255, 0, 0);
+        terminal::plot(0, 255, 0);
+        terminal::plot(0, 0, 255);
+        terminal::plot(255, 0, 0);
+        terminal::plot(0, 255, 0);
+        terminal::plot(0, 0, 255);
+        terminal::render()?;
+    }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,4 @@
-use std::process::Stdio;
+use std::{io::Write, process::Stdio};
 
 pub fn get_size() -> Option<(u16, u16)> {
     let out = std::process::Command::new("stty")
@@ -21,4 +21,32 @@ pub fn get_size() -> Option<(u16, u16)> {
         .expect("Failed to parse terminal size (cols)");
 
     Some((rows, cols))
+}
+
+pub fn clear() {
+    const CLEAR_ANSI: &str = "\x1B[2J";
+    const HOME_ANSI: &str = "\x1B[H";
+
+    print!("{CLEAR_ANSI}{HOME_ANSI}");
+}
+
+pub fn plot(r: u8, g: u8, b: u8) {
+    const BG_COLOR_ANSI: &str = "\x1B[48;2;";
+
+    print!("{BG_COLOR_ANSI}{r};{g};{b}m ");
+}
+
+pub fn render() -> std::io::Result<()> {
+    const BG_COLOR_ANSI: &str = "\x1B[48;2;";
+
+    print!("{BG_COLOR_ANSI}{};{};{}m", 0, 0, 0);
+    std::io::stdout().flush()
+}
+
+pub fn hide_cursor() {
+    print!("\x1B[?25l");
+}
+
+pub fn show_cursor() {
+    print!("\x1B[?25h");
 }
